8d5a129daf7ecc888501d32ca8c300005be1be9f
Matthieu Hostache <matthieu.hostache@gmail.com>
2021-07-22 11:36:39 +0200

    Still try to simplify the proxy conf

--- pages/api/services/[...args].js
+++ pages/api/services/[...args].js
@@ -36,26 +36,25 @@ const pathToApiProp = (prop) =>
 const router = pathToApiProp('baseUrl');
 const pathRewrite = pathToApiProp('newPath');
 
-const proxy = () =>
-  createProxyMiddleware(['/api/services/'], {
-    logLevel: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
-    target: 'https://jsonplaceholder.typicode.com',
-    // router,
-    // pathRewrite,
-    pathRewrite: { '^/api/proxy': '' },
-    changeOrigin: true,
-    // followRedirects: true,
-    // secure: false,
-    onProxyReq: (proxyReq) => {
-      proxyReq.socket.pause();
-      proxyReq.setHeader('x-random-header', 'random-value');
-      proxyReq.socket.resume();
-    },
-  });
+const proxy = createProxyMiddleware({
+  logLevel: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+  target: 'https://jsonplaceholder.typicode.com',
+  // router,
+  // pathRewrite,
+  pathRewrite: { '^/api/services': '' },
+  changeOrigin: true,
+  // followRedirects: true,
+  // secure: false,
+  onProxyReq: (proxyReq) => {
+    proxyReq.socket.pause();
+    proxyReq.setHeader('x-random-header', 'random-value');
+    proxyReq.socket.resume();
+  },
+});
 
 const handler = async (req, res) => {
   try {
-    return runMiddleware(req, res, proxy());
+    return runMiddleware(req, res, proxy);
   } catch (error) {
     res.status(error.status || 500).json({
       code: error.code,
